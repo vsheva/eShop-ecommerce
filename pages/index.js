093f95a85efde36afb36c5a1017b37aae3ea085f
vsheva <valera.sheva@gmail.com>
2022-11-04 06:33:39 +0200

    Add HeroBanner

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ import {Product, FooterBanner, HeroBanner} from '../components'
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">{['Product 1', 'Product2'].map(product => product)}</div>
-      Footer
+        <FooterBanner />
     </>
   );
 };
