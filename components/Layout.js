10a62a31cd6eab50e7d0d73ca077318bac428c15
vsheva <valera.sheva@gmail.com>
2022-11-05 03:51:49 +0200

    fix

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ const Layout = ({children}) => {
     return (
         <div className="layout">
             <Head>
-                <title> Store </title>
+                <title>ASV Store </title>
             </Head>
 
             <header>
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import React from "react"
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+  )}
 
 export default MyApp
