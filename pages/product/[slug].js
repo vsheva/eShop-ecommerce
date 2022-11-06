9faae677b8f2bc8ff4c298d31e48b62433907f40
vsheva <valera.sheva@gmail.com>
2022-11-06 21:33:59 +0200

    Add product details

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { client, urlFor } from '../../lib/client';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { Product } from '../../components';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
-
+  const [index, setIndex] = useState(0);
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} alt="" />
+            <img
+              src={urlFor(image && image[index])}
+              className="product-detail-image"
+              alt="headphone image"
+            />
           </div>
-          {/*<div className="small-images-container">
+          <div className="small-images-container">
             {image?.map((item, i) => (
-              <img src={urlFor(item)} className="" onMouseEnter="" />
+              <img
+                src={urlFor(item)}
+                className={i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)}
+              />
             ))}
-          </div>*/}
+          </div>
         </div>
 
         <div className="product-detail-desc">
@@ -63,13 +71,12 @@ const ProductDetails = ({ product, products }) => {
         <h2>You may also like</h2>
         <div className="marquee">
           <div className="maylike-products-container track">
-            {products.map((item) => (
-                <Product key={item._id} product={item} />
+            {products.map(item => (
+              <Product key={item._id} product={item} />
             ))}
           </div>
         </div>
       </div>
-
     </div>
   );
 };
