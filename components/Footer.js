83c3327715433b14e32c9fabbf735fc8ecf0c8f9
vsheva <valera.sheva@gmail.com>
2022-11-09 23:59:54 +0200

    Add payment with stripe

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import {AiFillInstagram, AiOutlineTwitter} from "react-icons/ai" //react icons  ///////////////////////////////////////////////////
-
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'; //react icons  ///////////////////////////////////////////////////
 
 const Footer = () => {
-    return (
-        <div className="footer-container">
-            <p> 2022 ASV Headphones All rights reserved</p>
-            <p className="icons">
-                <AiFillInstagram/>
-                <AiOutlineTwitter/>
-            </p>
-
-        </div>
-    );
+  return (
+    <div className="footer-container">
+      <p> 2022 ASV Headphones All rights reserved</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
